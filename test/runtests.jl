dea9d976a79c5036fd8be3c6c3d1ab207b5e0924
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2022-05-06 19:41:15 +0200

    Update test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,8 @@ end
     # quick test with `missing` weights.
     model_row = ModelRow(; weights=missing)
     write_model_row("my_model.model.arrow", model_row)
+    rt = read_model_row("my_model.model.arrow")
+    @test isequal(model_row, rt)
 
     model_row = ModelRow(; weights=collect(get_weights(my_model)))
     write_model_row("my_model.model.arrow", model_row)
